16443fa417932ca28cf370a7381aa184d720f737
lauberto <andrea.grillandi@live.it>
2021-10-21 10:33:41 +0300

    add argparser to train.py

--- glove/train.py
+++ glove/train.py
@@ -6,34 +6,30 @@ from glove import Corpus, Glove
 import tempfile
 import logging
 import os
-import configparser
-
-path_config_file = '/config.ini'
-config = configparser.ConfigParser()
-config.read(path_config_file)
-data_dir = config['PATHS']['data_dir']
-models_dir = config['PATHS']['models_dir']
-log_dir = config['PATHS']['log_dir']
-log_file = os.path.join(log_dir, 'training.txt')
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
-paths = [  
-  # Lemmatized w/ treetagger
-  os.path.join(data_dir, 'Economics.txt'),
-  os.path.join(data_dir, 'Education_and_psychology.txt'),
-  os.path.join(data_dir, 'History.txt'),
-  os.path.join(data_dir, 'Law.txt'),
-  os.path.join(data_dir, 'Linguistics.txt'),
-  os.path.join(data_dir, 'Sociology.txt'),
-  os.path.join(data_dir, 'supercybercat.txt'),
-
-]
+import argparse
 
 CONTEXT_WINDOW = 10 # 5, 10
-EPOCHS = 2
-SIZE = 50 # 200, 300, 500
+SIZE = 500 # 200, 300, 500
 
 def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--data-dir', type=str, help='Directory with corpus text files')
+  parser.add_argument('--models-dir', type=str, help='Directory where to store trained Glove models')
+  parser.add_argument('--size', default=500, type=int, help='Glove embedding size')
+  parser.add_argument('--context-window', default=10, type=int, help='Context window for the Glove model to consider')
+  args = parser.parser.parse_args()
+
+  paths = [  
+    # Lemmatized w/ treetagger
+    os.path.join(args.data_dir, 'Economics.txt'),
+    os.path.join(args.data_dir, 'Education_and_psychology.txt'),
+    os.path.join(args.data_dir, 'History.txt'),
+    os.path.join(args.data_dir, 'Law.txt'),
+    os.path.join(args.data_dir, 'Linguistics.txt'),
+    os.path.join(args.data_dir, 'Sociology.txt'),
+    os.path.join(args.data_dir, 'supercybercat.txt'),
+    ]
+
   for epochs in [30, 100]:
     for datapath in paths:
       print(f'Started training for: {datapath}', flush=True)
@@ -43,11 +39,11 @@ def main():
       corpus.fit(sentences, window=CONTEXT_WINDOW)
 
       glove = Glove(no_components=SIZE, learning_rate=0.05) 
-      glove.fit(corpus.matrix, epochs=EPOCHS, no_threads=40, verbose=True)
+      glove.fit(corpus.matrix, epochs=epochs, no_threads=40, verbose=True)
       glove.add_dictionary(corpus.dictionary)
-      glove.save(os.path.join(models_dir, model_name))
+      glove.save(os.path.join(args.models_dir, model_name))
 
-      glove_filename = os.path.join(models_dir, model_name)
+      glove_filename = os.path.join(args.models_dir, model_name)
       word2vec_output_file = glove_filename + '.word2vec'
       glove2word2vec(glove_filename, word2vec_output_file)
 
